eb776a38bc663ed5e1542072babd0bb08b28bf02
baoyan <271190187@qq.com>
2014-03-20 23:19:00 +0800

    12122

--- MYSQL.php
+++ MYSQL.php
@@ -30,7 +30,6 @@
                         <div class="panel-body">
                             <div class="panel panel-danger" id="user">
                                 <div class="panel-heading">Mysql 的ini文件配置</div>
-
                                 [mysql]<br>
                                 default-character-set=utf8
                                 <br>
@@ -47,15 +46,91 @@
                     <div class="col-md-10">
                         <div class="panel-body">
                             <div class="panel panel-danger" id="user">
+                                <div class="panel-heading">创建货物表</div>
+             <pre>
+               use ts_sys1;
+create table ts_good (
+
+    id         int           unsigned not null auto_increment  comment '//id',
+    pName      varchar(20)   not null                          comment '//商品名称',
+    pIns       varchar(50)                                     comment '//商品简介',
+    pOlodPrice mediumint(8)  unsigned                          comment '//商品原价',
+    pNewPrice  mediumint(8)  unsigned                          comment '//商品现价',
+    pReaUser   varchar(20)   not null                          comment '//商品发布者',
+    pMainImg   varchar(50)                                     comment '//商品主要图片',
+    pReaTime   datetime      not null                          comment '//商品发布日期',
+    beInTime   tinyint       unsigned                          comment '//商品上架天数',
+    PRIMARY KEY (id)
+
+)ENGINE=MyISAM DEFAULT CHARSET=utf8 auto_increment=50;
+             </pre>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class="col-md-10">
+                        <div class="panel-body">
+                            <div class="panel panel-danger" id="user">
+                                <div class="panel-heading">创建货物用户关联表</div>
+             <pre>
+               use ts_sys1;
+create table if not exists ts_userGood(
+
+  id         int           unsigned  not null  auto_increment comment '//关系id',
+  userName   varchar(20)             not null                 comment '//用户名',
+  goodId     int           unsigned  not null                 comment '//商品id',
+  buyCount   mediumint(8)  unsigned                           comment '//购买数量',
+  comment    varchar(2000)                                    comment '//用户评价',
+  comment1   varchar(2000)                                    comment '//用户追评',
+  userMark   char(1)                                          comment '//用户评分',
+  PRIMARY KEY (id)
+
+)ENGINE=MyISAM default charset = utf8 ;
+             </pre>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class="col-md-10">
+                        <div class="panel-body">
+                            <div class="panel panel-danger" id="user">
+                                <div class="panel-heading">创建触发器</div>
+             <pre>
+              create trigger t_setDir before insert on tg_user for each row
+              begin set NEW.tg_userDir = CONCAT('/users/',LEFT(NEW.tg_uniqid,10));
+                    set NEW.tg_reg_time = NOW();
+                    set NEW.tg_last_time = NOW();
+              end;
+             </pre>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class="col-md-10">
+                        <div class="panel-body">
+                            <div class="panel panel-danger" id="user">
                                 <div class="panel-heading">创建用户表</div>
              <pre>
-                    CREATE TABLE `tg_user` (
+                CREATE TABLE if not exists `tg_user` (
                 `tg_id` mediumint(8) unsigned NOT NULL auto_increment COMMENT '//用户自动编号',
                 `tg_uniqid` char(40) NOT NULL COMMENT '//验证身份的唯一标识符',
                 `tg_active` char(40) NOT NULL COMMENT '//激活登录用户',
                 `tg_username` varchar(20) NOT NULL COMMENT '//用户名',
                 `tg_password` char(40) NOT NULL COMMENT '//密码',
                 `tg_question` varchar(20) NOT NULL COMMENT '//密码提示',
+                `tg_userDir` varchar(50) NOT NULL COMMENT '//用户目录',
                 `tg_answer` char(40) NOT NULL COMMENT '//密码回答',
                 `tg_email` varchar(40) default NULL COMMENT '//邮件',
                 `tg_qq` varchar(10) default NULL COMMENT '//QQ',
